f251be36ae0193187fac6e2e5aad9f5e9432bf9e
Chris André <chris@chris-andre.fr>
2017-01-30 15:37:04 -0500

    Displayed the log in time for each user.

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,8 +1,12 @@
 package controllers;
 
+import akka.util.ByteString;
+import com.dynatrace.diagnostics.core.realtime.export.BtExport;
 import play.mvc.*;
 
 import views.html.*;
+
+import java.util.List;
 
 /**
  * This controller contains an action to handle HTTP requests
@@ -17,7 +21,25 @@ public class HomeController extends Controller {
      * <code>GET</code> request with a path of <code>/</code>.
      */
     public Result index() {
+        try {
+            System.err.println("Request received");
+            byte[] byteArray = request().body().asBytes().toArray();
+
+            BtExport.BusinessTransactions bts = BtExport.BusinessTransactions.parseFrom(byteArray);
+
+            List<BtExport.BusinessTransaction> businessTransactionList = bts.getBusinessTransactionsList();
+            for (BtExport.BusinessTransaction bt : businessTransactionList) {
+                for (BtExport.BtOccurrence occ : bt.getOccurrencesList()) {
+                    String message = String.format("%s - %s took %.3f ms to log in", bt.getName(), occ.getDimensions(0), occ.getResponseTime());
+
+                    System.err.println(message);
+                }
+            }
+            System.err.println("Request processed");
+        } catch (Exception exc) {
+            System.err.println("An error occured: " + exc);
+        }
+
         return ok("hello DT world");
     }
-
 }
